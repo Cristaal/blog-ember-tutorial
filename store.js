d82fc52590e93b8e9607afd6b352146848d362f0
Crystal Contreras-Grossman <crystalcontrerasgrossman@gmail.com>
2015-03-01 20:07:11 -0800

    add updated route

---
+++ store.js
@@ -0,0 +1,44 @@
+var posts = [
+  {
+    id: '1',
+    title: "I love blood",
+    body: "You know it."
+  },
+  {
+    id: '2',
+    title: "The Count",
+    body: "One! Ah ah ah ah ah!"
+  },
+  {
+    id: '3',
+    title: "Thievery",
+    body: "Thou shalt not steal."
+  }
+];
+var comments = [
+  {
+    id: '1',
+    postId: '3',
+    text: 'Sorry'
+  },
+  {
+    id: '2',
+    postId: '1',
+    text: 'Nope'
+  },
+  {
+    id: '3',
+    postId: '3',
+    text: 'Hey'
+  },
+  {
+    id: '4',
+    postId: '2',
+    text: 'Wow'
+  },
+  {
+    id: '5',
+    postId: '2',
+    text: 'Weird'
+  }
+]
